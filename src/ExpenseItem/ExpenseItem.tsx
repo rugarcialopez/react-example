126cef0b502c0dd9b331ed2279634b7d0daf5516
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2023-03-20 11:33:01 +0100

    First commit

---
+++ src/ExpenseItem/ExpenseItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <React.Fragment>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ExpenseItem;
